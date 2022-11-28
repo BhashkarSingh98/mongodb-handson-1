4231fbfe22fd49db342fe3461192f6b9c29f9f48
bhashkar <bhashkar2600@gmail.com>
2022-11-28 17:13:27 +0530

    fsfds

--- delete.js
+++ delete.js
@@ -4,7 +4,7 @@ const dbconnect=require("./mongodb")
 const deletedata=async ()=>{
 
     let data=await dbconnect();
-    let result=await data.deleteMany({lastCompany:"Y"})
+    let result=await data.deleteMany({lastCompany:"Z"})
     console.log(result);
     if(result.acknowledged){
         console.log("record deleted");
